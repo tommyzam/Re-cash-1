557e9ec5455f73886ae9520d30afac15ce29e922
Tommy Zamarripa <airbornekilo@gmail.com>
2020-02-04 19:55:53 -0600

    Fixed UI

--- app/src/main/java/com/example/re_cash/MainActivity.java
+++ app/src/main/java/com/example/re_cash/MainActivity.java
@@ -1,4 +1,14 @@
 package com.example.re_cash;
 
-public class MainActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
